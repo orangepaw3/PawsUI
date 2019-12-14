1043f41cdcf2fab5ebe367c718ef0811b2cfb945
orangepaw3 <candrid@gmail.com>
2019-12-14 17:24:45 -0500

    Added New Splash - Comments and Art

--- Modules/Features/splash.lua
+++ Modules/Features/splash.lua
@@ -1,9 +1,9 @@
 -- We need to add a frame
-local splashFrame = CreateFrame("frame",nil,UIParent,'BasicFrameTemplate');
-splashFrame:SetFrameStrata("LOW") -- Sets the Strata
-splashFrame:SetWidth(1024) -- Set these to whatever height/width is needed 
-splashFrame:SetHeight(512) -- for your Texture
-splashFrame:SetPoint("CENTER",0,0)
+local splashFrame = CreateFrame("frame",nil,UIParent);
+splashFrame:SetFrameStrata("LOW"); -- Sets the Strata
+splashFrame:SetWidth(1024); -- Set these to whatever height/width is needed 
+splashFrame:SetHeight(512); -- for your Texture
+splashFrame:SetPoint("CENTER",0,0);  -- needed to anchor to the screen
 splashFrame:SetAlpha(1);
 
 -- Texture
@@ -13,9 +13,19 @@ splashTexture:SetTexture("Interface\\Addons\\PawsUI\\Art\\splash.tga"); -- Sets 
 splashTexture:SetAllPoints(splashFrame); -- Sets all the points to "splashFrame."
 splashFrame.texture = splashTexture -- Identifies "splashFrame" texture to the variable name splashTexture.
 
+
+
+local eventManager = CreateFrame("Frame");
+eventManager:RegisterEvent("PLAYER_ENTERING_WORLD");
+eventManager:SetScript("OnEvent", function(self)
+	C_Timer.After(3, function()
+    	UIFrameFadeOut(splashFrame, 3, 1, 0);
+    end);
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD");	
+end);
+
 -- Slash Command to show splash screen
 SLASH_TOGGLESPLASH1 = "/splash"
 SlashCmdList.TOGGLESPLASH = function(arg) -- Splash display
 	print("|cffff0000Please eat more chicken.");
-    splashFrame:SetShown(not splashFrame:IsVisible())
 end

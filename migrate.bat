a99ebec637d7fc046ef3f880894d937e20f1a3f6
orangepaw3 <candrid@gmail.com>
2019-12-29 12:18:06 -0500

    Remarks Merge

--- migrate.bat
+++ migrate.bat
@@ -1,3 +1,6 @@
+REM Private Use - Will not function on other machines, if you need a migration let me know 
+REM and I can set you up a file to work regardless of user.
+
 set sourcedir=G:\PawsUI
 set targetdir=D:\World of Warcraft\_retail_\Interface\Addons
 xcopy "%sourcedir%" "%targetdir%"   /E /Y /Q
